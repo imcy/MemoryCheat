a7a3ed832a8c5ac88e729740d06a99260ea0ecf6
Chao ya <393632272@qq.com>
2017-12-08 19:57:01 +0800

    界面绘制

---
+++ MemoryCheat/MemoryCheatDlg.h
@@ -0,0 +1,33 @@
+
+// MemoryCheatDlg.h : 头文件
+//
+
+#pragma once
+
+
+// CMemoryCheatDlg 对话框
+class CMemoryCheatDlg : public CDialogEx
+{
+// 构造
+public:
+	CMemoryCheatDlg(CWnd* pParent = NULL);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MEMORYCHEAT_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
---
+++ MemoryCheat/resource.h
@@ -0,0 +1,28 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MemoryCheat.rc 使用
+//
+#define IDD_MEMORYCHEAT_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
+#define IDC_PROGRESS1                   1001
+#define IDC_EDIT1                       1004
+#define IDC_BUTTON2                     1005
+#define IDC_BUTTON3                     1006
+#define IDC_BUTTON4                     1007
+#define IDC_COMBO2                      1008
+#define IDC_EDIT2                       1009
+#define IDC_EDIT3                       1010
+#define IDC_HOTKEY1                     1014
+#define IDC_LIST1                       1015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1016
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
